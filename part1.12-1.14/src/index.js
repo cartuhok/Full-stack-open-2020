2119bb6612ee8a9944a2d2e4136d424b88a091e5
Carter Rink <carter.m.rink@gmail.com>
2020-10-27 21:02:37 -0400

    1.13 commit

--- part1.12-1.14/src/index.js
+++ part1.12-1.14/src/index.js
@@ -1,24 +1,6 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
 
-
-const Button = props => {
-  return (
-    <button onClick={props.handleEvent}>next anecdote</button>
-  )
-}
-
-const App = (props) => {
-  const [selected, setSelected] = useState(0)
-
-  return (
-    <div>
-      {props.anecdotes[selected]} <br />
-      <Button handleEvent={() => setSelected(Math.floor((Math.random() * props.anecdotes.length)))} />
-    </div>
-  )
-}
-
 const anecdotes = [
   'If it hurts, do it more often',
   'Adding manpower to a late software project makes it later!',
@@ -27,6 +9,45 @@ const anecdotes = [
   'Premature optimization is the root of all evil.',
   'Debugging is twice as hard as writing the code in the first place. Therefore, if you write the code as cleverly as possible, you are, by definition, not smart enough to debug it.'
 ]
+
+const Button = props => (
+  <>
+    <br />
+    <button onClick={props.handleEvent}>{props.text}</button>
+  </>
+)
+
+const Votes = ({votes}) => (
+  <>
+    <p>Has {votes} upvotes</p>
+  </>
+)
+
+const App = ({anecdotes}) => {
+  const [selected, setSelected] = useState(0)
+  const [points, setPoints] = useState(Array.apply(null, new Array(anecdotes.length)).map(Number.prototype.valueOf,0))
+  const score = [...points]
+  const randomNumber =  Math.floor(Math.random()*anecdotes.length)
+  
+
+  const handleNext = () => {
+    setSelected(randomNumber)
+  }
+
+  const handleVote = () => {
+    score[selected]+= 1
+    setPoints(score)
+  }
+
+  return (
+    <div>
+      {anecdotes[selected]}
+      <Button text="next quote" handleEvent={() => handleNext()} />
+      <Button text="upvote" handleEvent={() => handleVote()} />
+      <Votes votes={score[selected]} />
+    </div>
+  )
+}
 
 ReactDOM.render(
   <App anecdotes={anecdotes} />,
