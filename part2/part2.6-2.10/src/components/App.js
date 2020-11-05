68d0d7abdbf42202b3223ac2f6ec1cfb7b374ab9
Carter Rink <carter.m.rink@gmail.com>
2020-11-04 21:23:34 -0500

    2.9 commit

--- part2/part2.6-2.10/src/components/App.js
+++ part2/part2.6-2.10/src/components/App.js
@@ -1,13 +1,28 @@
 import React, { useState } from 'react'
 
-
 const App = () => {
   const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas', number: '040-1234567' }
+    { name: 'Arto Hellas', number: '040-123456' },
+    { name: 'Ada Lovelace', number: '39-44-5323523' },
+    { name: 'Dan Abramov', number: '12-43-234345' },
+    { name: 'Mary Poppendieck', number: '39-23-6423122' }
   ]) 
   const [ newName, setNewName ] = useState('')
-  const [newNumber, setNewNumber] = useState('')
+  const [ newNumber, setNewNumber ] = useState('')
+  const [ newSearch, setNewSearch ] = useState('')
 
+  const handleNameChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
+  
+  const handleSearchChange = (event) => {
+    setNewSearch(event.target.value)
+  }
+  
   const addName = (event) => {
     event.preventDefault()
 
@@ -29,22 +44,19 @@ const App = () => {
       setNewName('')
       setNewNumber('')
     }
-
     matchCheck.includes(true) ? alert() : handleNewNames()   
-
-  }
-
-  const handleNameChange = (event) => {
-    setNewName(event.target.value)
-  }
-
-  const handleNumberChange = (event) => {
-    setNewNumber(event.target.value)
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <div>
+          search contacts: <input 
+                  value={newSearch}
+                  onChange={handleSearchChange}   
+                />
+      </div>
+      <h2>Add New Contact:</h2>
       <form onSubmit={addName}>
         <div>
           name: <input 
@@ -62,9 +74,10 @@ const App = () => {
           <button type="submit">add</button>
         </div>
       </form>
+
       <h2>Numbers</h2>
       <div>
-        {persons.map(person => <p key={person.name}> {person.name}: {person.number}</p>)}
+      {persons.map(person => person.name.toLowerCase().includes(newSearch.toLowerCase()) ? <p key={person.name}>{person.name}: {person.number} </p> : null )}
       </div>
     </div>
   )
