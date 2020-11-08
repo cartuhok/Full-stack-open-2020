10f52d153c7e3829c2f3b119965e4af198b42d19
Carter Rink <carter.m.rink@gmail.com>
2020-11-08 18:48:47 -0500

    2.11 commit

--- part2/part2.6-2.10/src/components/People.js
+++ part2/part2.6-2.10/src/components/People.js
@@ -1,9 +1,10 @@
 import React from 'react'
 
 const People = ({persons, newSearch}) => {
+    const peopleArray = persons.map(person => person.name.toLowerCase().includes(newSearch.toLowerCase()) ? <p key={person.name}>{person.name}: {person.number} </p> : null )
     return (
         <div>
-            {persons.map(person => person.name.toLowerCase().includes(newSearch.toLowerCase()) ? <p key={person.name}>{person.name}: {person.number} </p> : null )}
+            {peopleArray}
         </div>
     )
 }
